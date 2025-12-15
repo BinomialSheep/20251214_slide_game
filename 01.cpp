84ff6aeafec90ea2e8c5fdcab2a7482d7a85994e
BinomialSheep <jose_jinrou@yahoo.co.jp>
2025-12-15 22:54:26 +0900

    fist commit

---
+++ 01.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+template <typename T> inline bool chmax(T &a, T b) {
+  return ((a < b) ? (a = b, true) : (false));
+}
+template <typename T> inline bool chmin(T &a, T b) {
+  return ((a > b) ? (a = b, true) : (false));
+}
+
+int main() {
+  //
+  cout << "Hello" << endl;
+}
